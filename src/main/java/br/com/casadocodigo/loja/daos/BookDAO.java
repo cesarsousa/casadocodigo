c723ad1957a7a3de0d1f462c19e80e97bb8f620c
cesar sousa <cesarsonline@gmail.com>
2019-07-15 23:16:33 -0300

    implementando...

--- src/main/java/br/com/casadocodigo/loja/daos/BookDAO.java
+++ src/main/java/br/com/casadocodigo/loja/daos/BookDAO.java
@@ -2,23 +2,37 @@ package br.com.casadocodigo.loja.daos;
 
 import java.util.List;
 
+import javax.ejb.Stateful;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
 
 import br.com.casadocodigo.loja.models.Book;
 
+@Stateful
 public class BookDAO {
-	
-	@PersistenceContext
+
+	@PersistenceContext(type=PersistenceContextType.EXTENDED)
 	private EntityManager manager;
-	
+
 	public void save(Book product) {
 		manager.persist(product);
 	}
-	
+
 	public List<Book> list() {
-		return manager.createQuery("select distinct(b) from Book b join fetch b.authors",
-				Book.class).getResultList();
+		return manager.createQuery("select distinct(b) from Book b join fetch b.authors", Book.class).getResultList();
+	}
+
+	public List<Book> lastReleases() {
+		return manager.createQuery("select b from Book b order by b.id desc", Book.class).setMaxResults(3).getResultList();
+	}
+
+	public List<Book> olderBooks() {
+		return manager.createQuery("select b from Book b", Book.class).setMaxResults(20).getResultList();
+	}
+
+	public Book findById(Integer id) {
+		return manager.find(Book.class, id);
 	}
 
 }
---
+++ src/main/java/br/com/casadocodigo/loja/managedbeans/site/ProductDetailBean.java
@@ -0,0 +1,35 @@
+package br.com.casadocodigo.loja.managedbeans.site;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+
+import br.com.casadocodigo.loja.daos.BookDAO;
+import br.com.casadocodigo.loja.models.Book;
+
+@Model
+public class ProductDetailBean {
+
+	@Inject
+	private BookDAO bookDAO;
+	private Book book;
+	private Integer id;
+
+	@PostConstruct
+	public void loadBook() {
+		this.book = bookDAO.findById(id);
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+}
